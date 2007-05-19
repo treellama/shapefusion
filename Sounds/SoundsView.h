02dfa63c8d8b35c609699a0fde0f4525f0f1e876
Tito Dal Canton <11317236+titodalcanton@users.noreply.github.com>
2007-05-19 17:17:06 +0000

    More work on the color table editor.

--- Sounds/SoundsView.h
+++ Sounds/SoundsView.h
@@ -16,8 +16,8 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-#ifndef __DRAWVIEW_H__
-#define __DRAWVIEW_H__
+#ifndef __SOUNDSVIEW_H__
+#define __SOUNDSVIEW_H__
 
 #include "wx/docview.h"
 #include "SoundsDocument.h"
