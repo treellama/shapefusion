426941182c26ca659a1d830b8ec5ffea8fc41219
rtoschi <rtoschi@ecd3d21b-4929-0410-96bb-c49030584e62>
2007-11-02 11:47:19 +0000

    Code cleanup, subclassed Big/LittleEndianBuffer from GenericEndianBuffer to remove code duplication.

--- trunk/LittleEndianBuffer.h
+++ trunk/LittleEndianBuffer.h
@@ -24,43 +24,25 @@
 #ifndef LITTLEENDIANBUFFER_H
 #define LITTLEENDIANBUFFER_H
 
-class LittleEndianBuffer {
-private:
-	unsigned char	*data,
-					*position;
-	unsigned int	size;
-	bool			self_allocated;
+#include "GenericEndianBuffer.h"
 
+class LittleEndianBuffer: public GenericEndianBuffer {
 public:
 	// self-allocate data buffer
 	LittleEndianBuffer(unsigned int _size);
 	// use a pre-allocated buffer
 	LittleEndianBuffer(unsigned char *_data, unsigned int _size);
 	~LittleEndianBuffer(void);
-	// set access position
-	void Position(unsigned int pos);
-	// return access position
-	unsigned int Position(void) const;
 	// read little-endian values, advance position accordingly
-	char ReadChar(void);
-	unsigned char ReadUChar(void);
 	short ReadShort(void);
 	unsigned short ReadUShort(void);
 	long ReadLong(void);
 	unsigned long ReadULong(void);
-	void ReadBlock(unsigned long _size, unsigned char *dest);
 	// write little-endian values, advance position accordingly
-	void WriteChar(char v);
-	void WriteUChar(unsigned char v);
 	void WriteShort(short v);
 	void WriteUShort(unsigned short v);
 	void WriteLong(long v);
 	void WriteULong(unsigned long v);
-	void WriteBlock(unsigned long _size, const void *src);
-	void WriteZeroes(unsigned int n);
-	// stuff access
-	unsigned char *Data(void) const;
-	unsigned int Size(void) const;
 };
 
 #endif
