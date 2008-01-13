8fc9ea0c4afe0a4914a3ed153724218a1365272d
Tito Dal Canton <11317236+titodalcanton@users.noreply.github.com>
2008-01-13 20:52:27 +0000

    Hack to hide the main frame on MacOS; about box version bump and fixes.

--- ShapeFusionApp.cpp
+++ ShapeFusionApp.cpp
@@ -23,30 +23,30 @@
 #include "SoundsDocument.h"
 #include "SoundsView.h"
 
-ShapeFusionMain *frame = (ShapeFusionMain *) NULL;
+ShapeFusionMain *frame = (ShapeFusionMain *)NULL;
 
 IMPLEMENT_APP(ShapeFusionApp)
 
 ShapeFusionApp::ShapeFusionApp(void)
 {
-    m_docManager = (wxDocManager *) NULL;
+    m_docManager = (wxDocManager *)NULL;
 }
 
 bool ShapeFusionApp::OnInit(void)
 {
-    if ( !wxApp::OnInit() )
-        return false;
+	if (!wxApp::OnInit())
+		return false;
 	
 	// so that we can import every sort of bitmap format
 	wxInitAllImageHandlers();
 		
 	// Create a document manager
-    m_docManager = new wxDocManager;
+	m_docManager = new wxDocManager;
 
-    // Create a template relating drawing documents to their views
-    (void) new wxDocTemplate(m_docManager, _T("Shapes"), _T("*"), _T(""), _T(""), _T("Shapes"), _T("Shapes"),
+	// Create a template relating drawing documents to their views
+	(void) new wxDocTemplate(m_docManager, _T("Shapes"), _T("*"), _T(""), _T(""), _T("Shapes"), _T("Shapes"),
 		CLASSINFO(ShapesDocument), CLASSINFO(ShapesView));
-    (void) new wxDocTemplate(m_docManager, _T("Sounds"), _T("*"), _T(""), _T(""), _T("Sounds"), _T("Sounds"),
+	(void) new wxDocTemplate(m_docManager, _T("Sounds"), _T("*"), _T(""), _T(""), _T("Sounds"), _T("Sounds"),
 		CLASSINFO(SoundsDocument), CLASSINFO(SoundsView));
 
 #ifdef __WXMAC__
@@ -56,21 +56,27 @@ bool ShapeFusionApp::OnInit(void)
 #endif
     
     // Create the main frame window
-    frame = new ShapeFusionMain(m_docManager, (wxFrame *) NULL, wxID_ANY, _T("ShapeFusion Workspace"), wxDefaultPosition, wxDefaultSize, wxDEFAULT_FRAME_STYLE);
-    
+#ifdef __WXMAC__
+	// a hack to make the frame invisible on MacOS, which is more Mac-like
+	// http://www.wxwidgets.org/wiki/index.php/WxMac_Issues#The_Mac_OS_menu_bar
+	frame = new ShapeFusionMain(m_docManager, (wxFrame *)NULL, wxID_ANY, _T("ShapeFusion Workspace"), wxPoint(5,5), wxSize(0,0), 0);
+#else
+    frame = new ShapeFusionMain(m_docManager, (wxFrame *)NULL, wxID_ANY, _T("ShapeFusion Workspace"), wxDefaultPosition, wxDefaultSize, wxDEFAULT_FRAME_STYLE);
+#endif
+
     // Give it an icon (this is ignored in MDI mode: uses resources)
 #ifdef __WXMSW__
     frame->SetIcon(wxIcon(_T("doc_icn")));
 #endif
 
     wxMenuBar *menu_bar = new wxMenuBar;
-    
+
 	CreateFileMenu(menu_bar);
 	CreateEditMenu(menu_bar);
 	CreateHelpMenu(menu_bar);
     
-    //// Associate the menu bar with the frame
-    frame->SetMenuBar(menu_bar);
+	// Associate the menu bar with the frame
+	frame->SetMenuBar(menu_bar);
 
 	//FIXME: This doesn't work
 	//wxMenuItem *menuItem = menu_bar->FindItem(FILE_MENU_HISTORY);
