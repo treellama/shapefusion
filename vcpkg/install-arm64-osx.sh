bc3ed1878fe4541df380f4940fad96d8964a7ce2
Gregory Smith <wolfy@treellama.org>
2026-03-30 21:52:15 -0400

    get macos build working with latest stable vcpkg baseline

---
+++ vcpkg/install-arm64-osx.sh
@@ -0,0 +1,1 @@
+`cat ~/.vcpkg/vcpkg.path.txt`/vcpkg --triplet=arm64-osx --overlay-triplets=custom-triplets --x-install-root=installed-arm64-osx --x-manifest-root=../VisualStudio/ install
---
+++ vcpkg/install-x64-osx.sh
@@ -0,0 +1,1 @@
+`cat ~/.vcpkg/vcpkg.path.txt`/vcpkg --triplet=x64-osx --overlay-triplets=custom-triplets --x-install-root=installed-x64-osx --x-manifest-root=../VisualStudio install
