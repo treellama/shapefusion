e9a5aa1c147f3a9f97a8d5c10ddbde400ca0cd8b
Gregory Smith <wolfy@treellama.org>
2026-03-31 22:07:06 -0400

    add a custom port for wxwidgets-3.2.10 and build against it (fixes Tahoe issues)

--- vcpkg/install-arm64-osx.sh
+++ vcpkg/install-arm64-osx.sh
@@ -1,1 +1,1 @@
-`cat ~/.vcpkg/vcpkg.path.txt`/vcpkg --triplet=arm64-osx --overlay-triplets=custom-triplets --x-install-root=installed-arm64-osx --x-manifest-root=../VisualStudio/ install
+`cat ~/.vcpkg/vcpkg.path.txt`/vcpkg --triplet=arm64-osx --overlay-triplets=custom-triplets --overlay-ports=custom-ports --x-install-root=installed-arm64-osx --x-manifest-root=../VisualStudio/ install
--- vcpkg/install-x64-osx.sh
+++ vcpkg/install-x64-osx.sh
@@ -1,1 +1,1 @@
-`cat ~/.vcpkg/vcpkg.path.txt`/vcpkg --triplet=x64-osx --overlay-triplets=custom-triplets --x-install-root=installed-x64-osx --x-manifest-root=../VisualStudio install
+`cat ~/.vcpkg/vcpkg.path.txt`/vcpkg --triplet=x64-osx --overlay-triplets=custom-triplets --overlay-ports=custom-ports --x-install-root=installed-x64-osx --x-manifest-root=../VisualStudio install
